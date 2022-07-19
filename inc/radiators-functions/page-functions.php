1458a07bcd88493c21a9366289dd4184e7a01198
«AndriiPoltava» <Omelianenko993@gmail.com>
2022-07-19 12:12:10 +0300

    preload image

--- inc/radiators-functions/page-functions.php
+++ inc/radiators-functions/page-functions.php
@@ -4,23 +4,42 @@
 
 	Preload the fatures image as it is used in the hero sections.
 \*---------------------------------------------------------------------------------*/
-function preload_featured_image(){
-	global $post;
+function preload_featured_image()
+{
+    global $post;
 
-	$term = "";
+    $term = "";
 
-	if (is_product_category()){
-		$term = get_queried_object();
-        $thumbnail_id = get_term_meta( $term->term_id, 'thumbnail_id', true );
+    if (is_product_category()) {
+        $term = get_queried_object();
+        $thumbnail_id = get_term_meta($term->term_id, 'thumbnail_id', true);
     }
 
-	
-	if ($thumbnail_id){
-		echo '<link rel="preload" as="image" href="' . wp_get_attachment_image_url($thumbnail_id,'full') . '" />';
-	} else if(has_post_thumbnail()){
-		$imageSrc = get_the_post_thumbnail_url($post, 'full');
-		echo '<link rel="preload" as="image" href="' . $imageSrc . '" />';
-	}
+
+    if ($thumbnail_id) {
+        if (wp_is_mobile()) {
+            echo '<link rel="preload" as="image" href="' . wp_get_attachment_image_url($thumbnail_id, 'woocommerce_thumbnail') . '" />';
+
+        } else {
+            echo '<link rel="preload" as="image" href="' . wp_get_attachment_image_url($thumbnail_id, 'woocommerce_single') . '" />';
+        }
+    } else if (has_post_thumbnail()) {
+        global $product;
+
+        if (is_product()) {
+            $attachment_ids = $product->get_gallery_image_ids();
+            if ($attachment_ids) {
+                echo '<link rel="preload" as="image" href="' . wp_get_attachment_url($attachment_ids[count($attachment_ids) - 1]) . '" />';
+            }
+
+        } else {
+
+            $imageSrc = get_the_post_thumbnail_url($post, 'medium');
+            echo '<link rel="preload" as="image" href="' . $imageSrc . '" />';
+        }
+
+
+    }
 
 }
 
