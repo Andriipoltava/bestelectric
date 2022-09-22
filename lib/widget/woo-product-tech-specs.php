bc8b016f4de5c2570313746ef6d66753730c4b9f
«AndriiPoltava» <Omelianenko993@gmail.com>
2022-09-22 09:47:29 +0300

    Add Guides section on product page

--- lib/widget/woo-product-tech-specs.php
+++ lib/widget/woo-product-tech-specs.php
@@ -93,6 +93,7 @@ class CustomWooProductTechSpecs extends Widget_Base
 
 
         $product_specification_title = get_field('product_specifications_title', $id);
+        $product_technical_specs_guides_title = get_field('product_technical_specs_guides_title', $id);
         $id = get_edit_id_page();
 
         global $product;
@@ -164,6 +165,15 @@ class CustomWooProductTechSpecs extends Widget_Base
                                             </li>
 
                                             <?php $counter_tab++; endwhile; ?>
+
+                                        <?php if ($product_technical_specs_guides_title && (have_rows('product_technical_specs_guides', $id) || have_rows('product_technical_specs_video', $id))) { ?>
+                                            <li class="c-product-tech-tabs-nav__item JS--product-tech-tab-nav "
+                                                data-spec-tab="<?php echo $counter_tab; ?>">
+                                        <span class="c-product-tech-tabs-nav__item-title"
+                                              title="<?php echo $product_technical_specs_guides_title; ?>"><?php echo $product_technical_specs_guides_title; ?></span>
+                                            </li>
+                                        <?php }; ?>
+
                                     </ul>
                                 </div>
                             </div>
@@ -228,7 +238,7 @@ class CustomWooProductTechSpecs extends Widget_Base
                                                 $cards = get_sub_field('product_technical_specs_card');
 
                                                 ?>
-                                                <div class="c-product-tech-accordion__content JS--product-specs-tab <?php echo (!$product_specification_title && $counter == 2) ? 'is-active' : null; ?><?php if (!get_sub_field('content_or_card') && $cards && count($cards)) echo ' c-product-tech-accordion__content__slider'?>"
+                                                <div class="c-product-tech-accordion__content JS--product-specs-tab <?php echo (!$product_specification_title && $counter == 2) ? 'is-active' : null; ?><?php if (!get_sub_field('content_or_card') && $cards && count($cards)) echo ' c-product-tech-accordion__content__slider' ?>"
                                                      data-spec-tab="<?php echo $counter; ?>">
 
                                                     <?php if ($content) {
@@ -240,7 +250,7 @@ class CustomWooProductTechSpecs extends Widget_Base
                                                                 <?php foreach ($cards as $key => $card) { ?>
                                                                     <div class="swiper-slide c-product-tech-accordion__installer__item">
                                                                         <div class="c-product-tech-accordion__installer__item__counter">
-                                                                            <?php echo $key+1; ?>
+                                                                            <?php echo $key + 1; ?>
                                                                         </div>
                                                                         <div class="c-product-tech-accordion__installer__item__text">
                                                                             <?php echo $card['title']; ?>
@@ -263,14 +273,65 @@ class CustomWooProductTechSpecs extends Widget_Base
                                             ?>
                                         <?php } ?>
 
+                                        <?php if (have_rows('product_technical_specs_guides', $id) || have_rows('product_technical_specs_video', $id)) : ?>
+                                            <div class="c-product-tech-accordion__content c-product-tech-accordion__content--full JS--product-specs-tab <?php echo (!$product_specification_title && $counter == 2) ? 'is-active' : null; ?><"
+                                                 data-spec-tab="<?php echo $counter; ?>">
+                                                <?php if (have_rows('product_technical_specs_guides', $id)) : ?>
 
+                                                    <div class="row product_technical_specs_guides">
+                                                        <?php
+                                                        while (have_rows('product_technical_specs_guides', $id)):
+                                                            the_row();
+                                                            $image = is_array(get_sub_field('image')) ? get_sub_field('image')['id'] : get_sub_field('image');
+                                                            $title = get_sub_field('title') ?: '';
+                                                            $link = get_sub_field('link') ?: "#";
+                                                            ?>
+                                                            <?php if ($image) { ?>
+                                                            <div class="col-xl-2 col-lg-3 col-md-4 col-6 ">
+                                                                <a href="<?php echo $link ?>" target="_blank"
+                                                                   class="product_guides__link">
+                                                                    <?php echo wp_get_attachment_image($image   , [164,228], null); ?>
+                                                                    <span class="product_guides__title"><?php echo $title; ?></span>
+                                                                </a>
+                                                            </div>
+                                                        <?php }; ?>
+                                                        <?php endwhile; ?>
+
+                                                    </div>
+                                                <?php endif; ?>
+                                                <?php if (have_rows('product_technical_specs_video', $id)) : ?>
+                                                    <div class="row product_technical_specs_video">
+                                                        <?php
+                                                        while (have_rows('product_technical_specs_video', $id)):
+                                                            the_row();
+                                                            $image = get_sub_field('image');
+                                                            $title = get_sub_field('title') ?: '';
+                                                            $link = get_sub_field('link') ?: "#";
+                                                            ?>
+                                                            <?php if ($image) { ?>
+                                                            <div class="col-xl-3 col-lg-4 col-6 text-center">
+                                                                <a href="<?php echo $link ?>" target="_blank"
+                                                                   class="product_guides__link">
+                                                                    <?php echo wp_get_attachment_image($image['id'], [281,178], null); ?>
+                                                                    <span class="product_guides__title"><?php echo $title; ?></span>
+                                                                </a>
+                                                            </div>
+                                                        <?php }; ?>
+                                                        <?php endwhile; ?>
+
+                                                    </div>
+                                                <?php endif; ?>
+                                            </div>
+
+                                        <?php endif; ?>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-
+                </div>
             </section>
+
         <?php endif;
         if (have_rows('product_technical_specs', $id) || have_rows('tech_specs_boxes', $id) || $product->get_upsell_ids()):
             echo "</div>";
